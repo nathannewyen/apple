ac4d54338db9b4b348a6365d472610b1303ee0ac
Nhan Nguyen <nhannguyen@Nhans-iMac.local>
2020-07-20 18:15:45 -0500

    update front-end

--- apple/src/components/Home.jsx
+++ apple/src/components/Home.jsx
@@ -1,18 +1,50 @@
-import React, { useState } from 'react';
-import { Link } from '@reach/router';
+import React, { useState } from "react";
+import { Link } from "@reach/router";
+import "../static/styles/css/home.scss";
 
-const buttonStyle = {
-	backgroundColor: '#3891DE',
-	padding: '15px 70px',
-	borderRadius: '5px',
-	border: '1px solid #0070C9'
-};
+import iPhoneSE from "../static/images/home/iPhoneSE.jpg";
+import iPhone11 from "../static/images/home/iPhone11.jpg";
+import TomHanks from "../static/images/home/tomHanks.jpg";
 
 const Home = (props) => {
-	return (
-		<>
-			</>
-	);
+  return (
+    <div className="home-wrapper">
+      <div className="iphoneSE text-center text-white">
+        <h1>iPhone SE</h1>
+        <h4>Lots to love. Less to spend.</h4>
+        <h4>Starting at $399</h4>
+        <p className="text-secondary">From $9.54/mo. or $299 trade-in.</p>
+        <Link to="#">
+          Learn More <i class="fal fa-chevron-right"></i>
+        </Link>{" "}
+        <Link to="#">
+          Buy <i class="fal fa-chevron-right"></i>
+        </Link>
+        <div className="container">
+          <img src={iPhoneSE} alt="iphone-SE" />
+        </div>
+      </div>
+      <div className="iphone11 text-center">
+        <h1>iPhone 11</h1>
+        <h4>Lots to love. Less to spend.</h4>
+        <h4>Starting at $399</h4>
+        <p className="text-secondary">From $9.54/mo. or $299 trade-in.</p>
+        <Link to="#">
+          Learn More <i class="fal fa-chevron-right"></i>
+        </Link>{" "}
+        <Link to="#">
+          Buy <i class="fal fa-chevron-right"></i>
+        </Link>
+        <div className="container-fluid">
+          <img src={iPhone11} alt="iphone-SE" />
+        </div>
+        <div className="tom-hank">
+          {/* <img src={TomHanks} alt="tom-hanks" /> */}
+          <h1>Hello</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- apple/src/components/Navbar.jsx
+++ apple/src/components/Navbar.jsx
@@ -1,81 +1,81 @@
-import React from 'react';
-import { Link } from '@reach/router';
-import theme from '../static/styles/theme';
-import FontFaces from '../static/styles/font';
-import NavbarStyle from '../static/styles/css/navbar.scss';
-
+import React from "react";
+import { Link } from "@reach/router";
+import "../static/styles/css/navbar.scss";
 
 const Navbar = (props) => {
-	return (
-		<nav className="navbar navbar-expand-lg">
-			<button
-				className="navbar-toggler"
-				type="button"
-				data-toggle="collapse"
-				data-target="#navbarSupportedContent"
-				aria-controls="navbarSupportedContent"
-				aria-expanded="false"
-				aria-label="Toggle navigation"
-			>
-				<span className="navbar-toggler-icon" />
-			</button>
+  return (
+    <nav className="navbar navbar-expand-lg navbar-default ">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
 
-			<div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
-				<ul className="navbar-nav">
-					<li className="nav-item active">
-						<Link className="navbar-brand" to="#">
-							<i className="fab fa-apple" />
-						</Link>
-					</li>
-					<li className="nav-item active">
-						<Link className="nav-link" to="#">
-							Mac <span class="sr-only">(current)</span>
-						</Link>
-					</li>
-					<li className="nav-item">
-						<Link className="nav-link" to="#">
-							iPad
-						</Link>
-					</li>
-					<li className="nav-item">
-						<Link className="nav-link" to="#">
-							iPhone
-						</Link>
-					</li>
-					<li className="nav-item">
-						<Link className="nav-link" to="#">
-							Watch
-						</Link>
-					</li>
-					<li className="nav-item">
-						<Link className="nav-link" to="#">
-							TV
-						</Link>
-					</li>
-					<li className="nav-item">
-						<Link className="nav-link" to="#">
-							Music
-						</Link>
-					</li>
-					<li className="nav-item">
-						<Link className="nav-link" to="#">
-							Support
-						</Link>
-					</li>
-					<li className="nav-item">
-						<Link className="nav-link" to="#">
-							<i className="fal fa-search" />
-						</Link>
-					</li>
-					<li className="nav-item">
-						<Link className="nav-link" to="#">
-							<i className="fal fa-shopping-bag" />
-						</Link>
-					</li>
-				</ul>
-			</div>
-		</nav>
-	);
+      <div
+        className="collapse navbar-collapse justify-content-center"
+        id="navbarSupportedContent"
+      >
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <Link className="navbar-brand" to="#">
+              <i className="fab fa-apple" />
+            </Link>
+          </li>
+          <li className="nav-item active">
+            <Link className="nav-link" to="#">
+              Mac <span class="sr-only">(current)</span>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="#">
+              iPad
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="#">
+              iPhone
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="#">
+              Watch
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="#">
+              TV
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="#">
+              Music
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="#">
+              Support
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="#">
+              <i className="fal fa-search" />
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="#">
+              <i className="fal fa-shopping-bag" />
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
