a7c1ae2f9c1f3a6ef759569e3739409c01c31bc4
Nhan Nguyen <nhannguyen@Nhans-iMac.local>
2020-07-22 22:44:36 -0500

    front-end

--- apple/src/components/Home.jsx
+++ apple/src/components/Home.jsx
@@ -1,110 +1,127 @@
-import React, { useState } from 'react';
-import { Link } from '@reach/router';
-import '../static/styles/css/home.scss';
+import React, { useState } from "react";
+import { Link } from "@reach/router";
+import "../static/styles/css/home.scss";
 
-import iPhoneSE from '../static/images/home/iPhoneSE.jpg';
-import All from '../static/images/home/All.jpg';
+import iPhoneSE from "../static/images/home/iPhoneSE.jpg";
+import All from "../static/images/home/All.jpg";
 
 const Home = (props) => {
-	return (
-		<div className="home-wrapper">
-			<div className="iphoneSE text-center text-white">
-				<h1>iPhone SE</h1>
-				<h4>Lots to love. Less to spend.</h4>
-				<h4>Starting at $399</h4>
-				<p className="text-secondary">From $9.54/mo. or $299 trade-in.</p>
-				<Link to="#">
-					Learn More <i class="fal fa-chevron-right" />
-				</Link>{' '}
-				<Link to="#">
-					Buy <i class="fal fa-chevron-right" />
-				</Link>
-				<div className="container">
-					<img src={iPhoneSE} alt="iphone-SE" />
-				</div>
-			</div>
-			<div className="iphone11 bg-white text-center">
-				<h1>iPhone 11</h1>
-				<h4>Lots to love. Less to spend.</h4>
-				<h4>Starting at $399</h4>
-				<p className="text-secondary">From $9.54/mo. or $299 trade-in.</p>
-				<Link to="#">
-					Learn More <i class="fal fa-chevron-right" />
-				</Link>{' '}
-				<Link to="#">
-					Buy <i class="fal fa-chevron-right" />
-				</Link>
-				<div className="container">
-					<img
-						src="https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQBHxprlRjg0G-eFKuCuVlNG0oSaz9Gy0j6PyJIUiOIoQthK_k1bAY&usqp=CAc"
-						alt="iphone-11"
-						height="450"
-						width="100%"
-					/>
-				</div>
-			</div>
+  return (
+    <div className="home-wrapper">
+      <div className="iphoneSE text-center text-white">
+        <h1>iPhone SE</h1>
+        <h4>Lots to love. Less to spend.</h4>
+        <h4>Starting at $399</h4>
+        <p className="text-secondary">From $9.54/mo. or $299 trade-in.</p>
+        <Link to="#">
+          Learn More <i class="fal fa-chevron-right" />
+        </Link>{" "}
+        <Link to="#">
+          Buy <i class="fal fa-chevron-right" />
+        </Link>
+        <div className="container">
+          <img src={iPhoneSE} alt="iphone-SE" />
+        </div>
+      </div>
+      <div className="iphone11 bg-white text-center">
+        <h1>iPhone 11</h1>
+        <h4>Lots to love. Less to spend.</h4>
+        <h4>Starting at $399</h4>
+        <p className="text-secondary">From $9.54/mo. or $299 trade-in.</p>
+        <Link to="#">
+          Learn More <i class="fal fa-chevron-right" />
+        </Link>{" "}
+        <Link to="#">
+          Buy <i class="fal fa-chevron-right" />
+        </Link>
+        <div className="container">
+          <img
+            src="https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQBHxprlRjg0G-eFKuCuVlNG0oSaz9Gy0j6PyJIUiOIoQthK_k1bAY&usqp=CAc"
+            alt="iphone-11"
+            height="450"
+            width="100%"
+          />
+        </div>
+      </div>
 
-			<div className="ads">
-				<div className="row text-center">
-					<div className="col-md-6">
-						<div class="col-md-12 bg-light">
-							<h1>Apple Card Monthly Installments</h1>
-							<img src={All} alt="all" width="400px" />
-						</div>
-					</div>
-					<div className="col-md-6 ">
-						<div class="col-md-12 bg-light">
-							<h1>Apple Card Monthly Installments</h1>
-							<img
-								src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Apple_logo_dark_grey.svg/1200px-Apple_logo_dark_grey.svg.png"
-								alt="all"
-								width="160"
-							/>
-						</div>
-					</div>
-				</div>
-			</div>
+      <div className="ads">
+        <div className="row text-center">
+          <div className="col-md-6">
+            <div class="col-md-12 bg-light">
+              <h1>Apple Card Monthly Installments</h1>
+              <p>
+                <Link to="#">
+                  Learn more <i class="fal fa-chevron-right" />
+                </Link>
+              </p>
+              <img src={All} alt="all" width="400px" />
+            </div>
+          </div>
+          <div className="col-md-6 ">
+            <div class="col-md-12 bg-light">
+              <h1>Apple Approach to repoening stores</h1>
+              <p>
+                <Link to="#">
+                  Read the letter <i class="fal fa-chevron-right" />
+                </Link>
+              </p>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Apple_logo_dark_grey.svg/1200px-Apple_logo_dark_grey.svg.png"
+                alt="all"
+                width="200"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
 
-			{/* Footer */}
-			<div className="footer">
-				<div className="container text-justify">
-					<p>
-						1.Must be at least 18. Apple or its trade-in partners reserve the right to refuse or limit any
-						Trade In transaction for any reason. In‑store trade‑in requires presentation of a valid,
-						government-issued photo ID (local law may require saving this information). Sales tax may be
-						assessed on full value of new iPhone. Additional terms from Apple or Apple’s trade-in partners
-						may apply. Monthly pricing: Available to qualified customers and requires 0% APR, 24-month
-						installment loan with Citizens One or Apple Card Monthly Installments and iPhone activation with
-						AT&T, Sprint, T-Mobile, or Verizon. Taxes and shipping not included. Additional Apple Card
-						Monthly Installments terms are in the Customer Agreement. Additional iPhone Payments terms are
-						here.
-						<br />
-						<br />
-						2. $4.99/month after free trial. No commitment. Plan automatically renews after trial until
-						cancelled. Magic Keyboard sold separately. Apple TV+ is $4.99/month after free trial. One
-						subscription per Family Sharing group. Offer good for 3 months after eligible device activation.
-						Plan automatically renews until cancelled. Restrictions and other terms apply.
-					</p>
-					<hr />
-					<div className="row">
-						<div className="col-4">Copyright © 2020 Apple Inc. All rights reserved.</div>
-						<div className="col-5">
-							<Link to="#"> Privacy Policy</Link>
-							{' | '}
-							<Link to="#"> Terms of Use </Link>
-							{' | '}
-							<Link to="#"> Sales and Refunds </Link>
-							{' | '}
-							<Link to="#"> Legal </Link>
-							{' | '}
-							<Link to="#"> Site Map </Link>
-						</div>
-						<div className="col-3 text-right">United States</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+      {/* Footer */}
+      <div className="footer">
+        <div className="container text-justify">
+          <p>
+            1.Must be at least 18. Apple or its trade-in partners reserve the
+            right to refuse or limit any Trade In transaction for any reason.
+            In‑store trade‑in requires presentation of a valid,
+            government-issued photo ID (local law may require saving this
+            information). Sales tax may be assessed on full value of new iPhone.
+            Additional terms from Apple or Apple’s trade-in partners may apply.
+            Monthly pricing: Available to qualified customers and requires 0%
+            APR, 24-month installment loan with Citizens One or Apple Card
+            Monthly Installments and iPhone activation with AT&T, Sprint,
+            T-Mobile, or Verizon. Taxes and shipping not included. Additional
+            Apple Card Monthly Installments terms are in the Customer Agreement.
+            Additional iPhone Payments terms are here.
+            <br />
+            <br />
+            2. $4.99/month after free trial. No commitment. Plan automatically
+            renews after trial until cancelled. Magic Keyboard sold separately.
+            Apple TV+ is $4.99/month after free trial. One subscription per
+            Family Sharing group. Offer good for 3 months after eligible device
+            activation. Plan automatically renews until cancelled. Restrictions
+            and other terms apply.
+          </p>
+          <hr />
+          <div className="row">
+            <div className="col-4">
+              Copyright © 2020 Apple Inc. All rights reserved.
+            </div>
+            <div className="col-5">
+              <Link to="#"> Privacy Policy</Link>
+              {" | "}
+              <Link to="#"> Terms of Use </Link>
+              {" | "}
+              <Link to="#"> Sales and Refunds </Link>
+              {" | "}
+              <Link to="#"> Legal </Link>
+              {" | "}
+              <Link to="#"> Site Map </Link>
+            </div>
+            <div className="col-3 text-right">United States</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- apple/src/components/Mac.jsx
+++ apple/src/components/Mac.jsx
@@ -11,7 +11,30 @@ const Mac = (props) => {
       </nav>
 
       <div className="container">
-        <h3>Choose your new MacBook Pro.</h3>
+        <h3 className="text-center">Choose your new MacBook Pro.</h3>
+        <div className="card border-0">
+          <div className="card-body text-center">
+            <img
+              src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp13touch-space-select-202005?wid=904&hei=840&fmt=jpeg&qlt=80&op_usm=0.5,0.5&.v=1587460552755"
+              alt="mac"
+              width=" 250"
+            />
+            <div class="check-box">
+              <input type="radio" />
+            </div>
+
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#!" class="card-link">
+              Card link
+            </a>
+            <a href="#!" class="card-link">
+              Another link
+            </a>
+          </div>
+        </div>
       </div>
     </>
   );
